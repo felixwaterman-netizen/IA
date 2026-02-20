287b468a7627c0e850efae8f5f6667dc8c95cd2e
felixwaterman-netizen <felixwaterman@students.berkeley.net>
2026-02-20 19:25:02 +0000

    first commit

--- script.js
+++ script.js
@@ -1,37 +1,9 @@
 function run() {
 
-    //keep the existing entity parameter
-    //add term and limit parameters using user entered values
-    //additional parameters are connected with "&"
-    let url = "https://itunes.apple.com/search?entity=musicTrack
-
-    //connects to iTunes api and passes off to process function on success
-    $.ajax({
-        url: url,
-        dataType: "json",
-        success: process
-    });
+    console.log("HI")
     
 }
 
-function process(data) {
-    console.log(data)
+function process() {
 
-    let o = "";
-
-    //builds a giant html string - you need to add the fields 
-    //from iTunes into the table and then....
-    for(let p=0;p<data.results.length;p++) {
-        o += "<tr>";
-        o += "<td>" + + "</td>";
-        o += "<td>" + + "</td>";
-        o += "<td>" + + "</td>";
-        o += "<td></td>";
-        o += "</tr>";
-    }
-
-    //adds the created table to the page
-    let table = document.getElementById("output");
-    table.innerHTML = o;
-    table.style.display = "table";
 }
